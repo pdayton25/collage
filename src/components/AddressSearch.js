465495fc5c1647476657adb0c8b05fa866431d47
Patrick Dayton <patrick.dayton24@gmail.com>
2022-04-12 13:11:09 -0500

    implemented grid layout and created overlay for each NFT

--- src/components/AddressSearch.js
+++ src/components/AddressSearch.js
@@ -4,7 +4,7 @@ import Dropdown from "./Dropdown";
 import "./AddressSearch.css";
 
 const wallets = [
-    "0x4716d4596621dfaf15c6f91c8d5f378b3e49b605","0x3f3095e5Fd52143F47fD4A89210b79127f62D07C","0xCa133474Fc6239FdFEb1f4bd5B3F75fAf3182e08", "0xA7Bf75908314A1020352C36d8B029d34721a83fd", "0xa1AC1D6731bE5bB94BB8319D63E3b2908ac550Eb", "0x3eD165F81394A2c7032Cb2Ba9ab7B376e5EF4180","0xf5C86499385a1E594523bEBaE3E0bbCf64bA8936", "0x0E8E616083529980e13F66d3Bb3e859E2451d160", "0x16a16F056dBa6Eb85A989B9d6B6E99Bf3BA480FD"
+    "0x4716d4596621dfaf15c6f91c8d5f378b3e49b605", "0x3f3095e5Fd52143F47fD4A89210b79127f62D07C","0xCa133474Fc6239FdFEb1f4bd5B3F75fAf3182e08", "0xA7Bf75908314A1020352C36d8B029d34721a83fd", "0xa1AC1D6731bE5bB94BB8319D63E3b2908ac550Eb", "0x3eD165F81394A2c7032Cb2Ba9ab7B376e5EF4180","0xf5C86499385a1E594523bEBaE3E0bbCf64bA8936", "0x0E8E616083529980e13F66d3Bb3e859E2451d160", "0x16a16F056dBa6Eb85A989B9d6B6E99Bf3BA480FD"
 ]
 console.log(wallets.length)
 
@@ -35,7 +35,7 @@ const randomAddress = (e) => {
                     <input
                         type="text"
                         className="address-field search-bar"
-                        placeholder="enter wallet address"
+                        placeholder="enter wallet address (ENS supported)"
                         value={searchAddress}
                         onChange= {(e) => setSearchAddr(e.target.value)}
                     />
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,6 +4,7 @@ const Dropdown = () => {
     return (
         <select className="dropdown search-bar">
             <option className="option">Ethereum</option>
+            <option className="option coming-soon">Polygon (Coming Soon)</option>
             <option className="option coming-soon">Solana (Coming Soon)</option>
             <option className="option coming-soon">NEAR (Coming Soon)</option>
         </select>
