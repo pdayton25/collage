690778bba74936d5fcc79f118882b62c94bee633
Patrick Dayton <patrick.dayton24@gmail.com>
2022-04-13 20:25:37 -0500

    add display component for future features

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddressSearch from './components/AddressSearch';
 import Header from './components/Header';
+import Display from './components/Display';
 import "./App.css";
 
 
 const App = () => {
 
+    const [searchAddress, setSearchAddress] = useState('')
+    const [dropdownSelection, setDropdownSelection] = useState('')
+    console.log(searchAddress);
+
     return (
         <div className='body'>
             <Header />
-            <AddressSearch />
+            <AddressSearch
+                passSearchAddress= {setSearchAddress}
+                passDropdownSelection= {setDropdownSelection}
+            />
+            <Display />
         </div>
     );
 };
