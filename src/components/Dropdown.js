15b1772399460f681b22938b3b4a9ecc59748822
Patrick Dayton <patrick.dayton24@gmail.com>
2022-08-09 21:57:16 -0500

    updated alchemy api

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -8,9 +8,9 @@ const Dropdown = (props) => {
     return (
         <select className="dropdown search-bar" onChange={(e) => {setSelected(e.target.value)}} >
             <option  className="option" value="ethereum" >Ethereum</option>
-            <option className="option coming-soon" value="solana">Solana</option>
-            <option className="option coming-soon" value="polygon">Polygon (Coming Soon)</option>
-            <option className="option coming-soon" value="near">NEAR (Coming Soon)</option>
+            <option className="option coming-soon" value="solana" disabled='disabled'>Solana (Coming Soon)</option>
+            <option className="option coming-soon" value="polygon" disabled='disabled'>Polygon (Coming Soon)</option>
+            <option className="option coming-soon" value="near" disabled='disabled'>NEAR (Coming Soon)</option>
         </select>
     )
 }
